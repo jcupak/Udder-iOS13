39c2fed2abb6ae2431381d5dcdbfacd01bc6fc61
John J Cupak Jr, CCP <john.cupak@me.com>
2020-03-09 13:14:36 -0400

    Initial Commit

---
+++ Udder-iOS13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder-iOS13
+//
+//  Created by John Cupak on 3/9/20.
+//  Copyright © 2020 John Cupak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
